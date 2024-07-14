4b72c3dea89afa621ac177a0c38ce982117f6bfb
bdlm-dev <74147859+bdlm-dev@users.noreply.github.com>
2024-07-14 21:02:34 +0100

    Only randomize HarvestPOI locations if that config is true

--- Patchers/GameSceneInitializerPatcher.cs
+++ Patchers/GameSceneInitializerPatcher.cs
@@ -12,21 +12,24 @@ public class GameSceneInitializerPatcher
     // TODO: Add an event to DredgeEvents to replace the need for this patch. GameSceneInitializer.Start is a good canditate for a new event
     public static void Prefix(GameSceneInitializer __instance)
     {
-        try
+        if (RandomizerConfig.Instance.RandomizeHarvestPoIs)
         {
-            var harvestPoisContainer = GameObject.Find("HarvestPOIs");
-            if (harvestPoisContainer == null)
+            try
             {
-                WinchCore.Log.Error("HarvestPOIs not found");
-                return;
-            }
+                var harvestPoisContainer = GameObject.Find("HarvestPOIs");
+                if (harvestPoisContainer == null)
+                {
+                    WinchCore.Log.Error("HarvestPOIs not found");
+                    return;
+                }
 
-            var poiList = PoiHelpers.GetPoiListFromContainer(harvestPoisContainer);
-            PoiHelpers.PermuteHarvestPoiLocations(poiList);
-        }
-        catch (Exception ex)
-        {
-            WinchCore.Log.Error($"Error in {nameof(GameSceneInitializerPatcher)}: exception {ex}");
+                var poiList = PoiHelpers.GetPoiListFromContainer(harvestPoisContainer);
+                PoiHelpers.PermuteHarvestPoiLocations(poiList);
+            }
+            catch (Exception ex)
+            {
+                WinchCore.Log.Error($"Error in {nameof(GameSceneInitializerPatcher)}: exception {ex}");
+            }
         }
     }
 }
